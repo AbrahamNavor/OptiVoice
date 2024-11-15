a8f04c1f877e0e13e69ed51861d2826d6c1c565d
mgfuentess <mariaguadalupefuentes9917@gmail.com>
2024-11-15 10:32:23 -0600

    cambio en las rutas

--- OptiVoice/routes/web.php
+++ OptiVoice/routes/web.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::view('/panel','panel')->name('rutaPanel');
 Route::view('/creacion_tareas','creacion_tareas')->name('rutaCreacion'); */
 
-Route::get('/', [controladorVista::class,'inicio'])->name('rutaInicio');
+Route::get('/inico', [controladorVista::class,'inicio'])->name('rutaInicio');
 Route::get('/panel', [controladorVista::class,'panel'])->name('rutaPanel'); 
 Route::get('/creacion_tareas', [controladorVista::class,'creacion_tareas'])->name('rutaCreacion');
-Route::get('/index', [controladorVista::class,'index'])->name('rutaIndex');
+Route::get('/', [controladorVista::class,'index'])->name('rutaIndex');
 Route::get('/sesion', [controladorVista::class,'sesion'])->name('rutaSesion');
 Route::get('/nueva_cuenta', [controladorVista::class,'nueva_cuenta'])->name('rutaNuevaCuenta');
 
